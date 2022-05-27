fd6f222c6e33dbc77c983bd2924ea019df064f4d
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-05-27 22:34:59 +0300

    change

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,5 +1,5 @@
 function fetchImagesAPI(keyword, page) {
-  const API_KEY = '24760517-308a82b19588512bdc36b5eaa';
+  const API_KEY = '24760517-308a82b19588512bdc36b5e90';
   const URL = `https://pixabay.com/api/?q=${keyword}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
   return fetch(URL).then(response => {
