743aac71c06934d2966e1323bbd305ca434dea64
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-05-27 22:19:55 +0300

    add hooks

---
+++ src/js/fetchAPI.js
@@ -0,0 +1,15 @@
+function fetchImagesAPI(keyword, page) {
+  const API_KEY = '24760517-308a82b19588512bdc36b5eaa';
+  const URL = `https://pixabay.com/api/?q=${keyword}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+  return fetch(URL).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(
+      new Error(`Картинок по запросу ${keyword} не найдено`),
+    );
+  });
+}
+
+export default fetchImagesAPI;
